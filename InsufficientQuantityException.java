b1eeec58805a9f26ea27d43a707752d051bc7642
Antoan Mihaylov <35418257+axelbladers@users.noreply.github.com>
2024-06-03 20:08:26 +0300

    Update InsufficientQuantityException.java

--- InsufficientQuantityException.java
+++ InsufficientQuantityException.java
@@ -1,11 +1,5 @@
-package Shop;
 public class InsufficientQuantityException extends Exception {
-    /**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-
-	public InsufficientQuantityException(String message) {
+    public InsufficientQuantityException(String message) {
         super(message);
     }
 }
