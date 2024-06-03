2c6f09a02a0d837a03e2b5d86619e7dc037fa671
Antoan Mihaylov <35418257+axelbladers@users.noreply.github.com>
2024-06-03 20:09:38 +0300

    Update Store.java

--- Store.java
+++ Store.java
@@ -1,80 +1,71 @@
-package Shop;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Store {
+    private List<Product> products;
     private List<Cashier> cashiers;
-    private List<Product> deliveredProducts;
-    private List<Product> soldProducts;
     private List<Receipt> receipts;
-    private double totalRevenue;
-    private double totalExpenses;
+    private double expenses;
+    private static int daysThreshold;
 
-    public Store() {
-        this.cashiers = new ArrayList<>();
-        this.deliveredProducts = new ArrayList<>();
-        this.soldProducts = new ArrayList<>();
-        this.receipts = new ArrayList<>();
-        this.totalRevenue = 0.0;
-        this.totalExpenses = 0.0;
+    public Store(int daysThreshold) {
+        products = new ArrayList<>();
+        cashiers = new ArrayList<>();
+        receipts = new ArrayList<>();
+        expenses = 0;
+        Store.daysThreshold = daysThreshold;
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+        expenses += product.getDeliveryPrice() * product.getQuantity();
     }
 
     public void addCashier(Cashier cashier) {
         cashiers.add(cashier);
-        totalExpenses += cashier.getMonthlySalary();
+        expenses += cashier.getSalary();
     }
 
-    public void addDeliveredProduct(Product product, int quantity) {
-        deliveredProducts.add(new Product(product.getName(), product.getPrice(), quantity));
-        totalExpenses += product.getPrice() * quantity;
-    }
-
-    public void addSoldProduct(Product product, int quantity) {
-        soldProducts.add(new Product(product.getName(), product.getPrice(), quantity));
-    }
-
-    public void addReceipt(Receipt receipt) throws IOException {
+    public void addReceipt(Receipt receipt) {
         receipts.add(receipt);
-        totalRevenue += receipt.getTotalPrice();
-        Store.saveReceiptToFile(receipt);
-    }
-
-    public double calculateProfit() {
-        return totalRevenue - totalExpenses;
-    }
-
-    public double getTotalRevenue() {
-        return totalRevenue;
-    }
-
-    public double getTotalExpenses() {
-        return totalExpenses;
+        receipt.saveToFile();
     }
 
     public int getTotalReceipts() {
         return receipts.size();
     }
 
-    public static void saveReceiptToFile(Receipt receipt) throws IOException {
-        String filename = "receipt_" + receipt.getReceiptNumber() + ".txt";
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
-            writer.write(receipt.toString());
-        }
+    public double getTurnover() {
+        return receipts.stream().mapToDouble(Receipt::calculateTotal).sum();
     }
 
-    public static String readReceiptFromFile(int receiptNumber) throws IOException {
-        String filename = "receipt_" + receiptNumber + ".txt";
-        return new String(Files.readAllBytes(Paths.get(filename)));
+    public static int getDaysThreshold() {
+        return daysThreshold;
     }
 
-	public void addSoldProduct(Receipt receipt) {
-		// TODO Auto-generated method stub
-		
-	}
+    public List<Product> getProducts() {
+        return products;
+    }
 
+    public List<Cashier> getCashiers() {
+        return cashiers;
+    }
+
+    public List<Receipt> getReceipts() {
+        return receipts;
+    }
+
+    public double getExpenses() {
+        double productExpenses = products.stream()
+                .mapToDouble(product -> product.getDeliveryPrice() * product.getQuantity())
+                .sum();
+        double cashierExpenses = cashiers.stream()
+                .mapToDouble(Cashier::getSalary)
+                .sum();
+        return productExpenses + cashierExpenses;
+    }
+
+    public double getProfit() {
+        return getTurnover() - getExpenses();
+    }
 }
